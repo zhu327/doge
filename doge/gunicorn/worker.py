54ba17f9e997e468ab0d7af15e5ef7f45b19e3d6
Timmy <zhu327@gmail.com>
2018-09-30 17:56:31 +0800

    Update worker.py

--- doge/gunicorn/worker.py
+++ doge/gunicorn/worker.py
@@ -7,4 +7,5 @@ class DogeWorker(GeventWorker):
     u""" 适用于Doge的Gunicorn Worker
     """
     def handle(self, listener, client, addr):
+        client.setblocking(1)
         self.wsgi.handler(client, addr)
