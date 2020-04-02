a069dc78b91021f704270f4426b0ef41b324c131
Timmy <zhu327@gmail.com>
2020-04-02 01:54:28 +0000

    rename metric to metrics

--- doge/filter/metric/__init__.py
+++
@@ -1,5 +0,0 @@
-from doge.filter.metric.client import MetricClientFilter
-from doge.filter.metric.server import MetricServerFilter
-
-
-__all__ = [MetricClientFilter, MetricServerFilter]
---
+++ doge/filter/metrics/__init__.py
@@ -0,0 +1,5 @@
+from doge.filter.metrics.client import MetricClientFilter
+from doge.filter.metrics.server import MetricServerFilter
+
+
+__all__ = [MetricClientFilter, MetricServerFilter]
